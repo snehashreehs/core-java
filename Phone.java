66311df2d7e642ee2e5d7579f3a7180241f276a2
snehashree h s <ssnehashreehs@gmail.com>
2026-03-30 12:16:37 +0530

    task8

---
+++ Phone.java
@@ -0,0 +1,59 @@
+class Phone {
+    static String brand = "Apple";
+    static int price = 80000;
+    static String color = "Black";
+}
+
+class Laptop {
+    static String brand = "Dell";
+    static int price = 60000;
+    static String processor = "i5";
+}
+
+class TV {
+    static String brand = "Samsung";
+    static int size = 55;
+    static String type = "LED";
+}
+
+class Watch {
+    static String brand = "Titan";
+    static int price = 5000;
+    static String type = "Analog";
+}
+
+class Tablet {
+    static String brand = "Lenovo";
+    static int price = 25000;
+    static String storage = "128GB";
+}
+
+class Camera {
+    static String brand = "Canon";
+    static int price = 45000;
+    static String resolution = "24MP";
+}
+
+class Speaker {
+    static String brand = "Sony";
+    static int price = 7000;
+    static String type = "Bluetooth";
+}
+
+class Refrigerator {
+    static String brand = "LG";
+    static int price = 30000;
+    static int capacity = 260;
+}
+
+class WashingMachine {
+    static String brand = "Whirlpool";
+    static int price = 20000;
+    static String type = "Front Load";
+}
+
+class AirConditioner {
+    static String brand = "Voltas";
+    static int price = 35000;
+    static int ton = 1;
+}
